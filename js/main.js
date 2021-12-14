b1d1964afed751775c827770ba21f2087ea5364d
Rehan Siraj <rsiraj@r2hstudent.org>
2021-12-13 20:38:38 -0500

    updated product page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+let galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    }
+});
